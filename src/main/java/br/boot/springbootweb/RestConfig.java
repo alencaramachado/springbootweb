f9ac65bbc41ac254ed09f8743e517113e7ed3e5d
Alencar Machado <alencar.machado@ufsm.br>
2018-10-02 13:29:19 -0300

    add corsfilter

---
+++ src/main/java/br/boot/springbootweb/RestConfig.java
@@ -0,0 +1,39 @@
+package br.boot.springbootweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class RestConfig {
+
+		@Bean
+	    public CorsFilter corsFilter() {
+	 
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	 
+	        CorsConfiguration config = new CorsConfiguration();
+	        config.setAllowCredentials(true);
+	        config.addAllowedOrigin("*");
+	        config.addAllowedHeader("*");
+	       
+	        config.addAllowedHeader("Authorization");
+	        config.addExposedHeader("Authorization");
+	        config.addAllowedHeader("Control-Allow-Headers");
+	        config.addExposedHeader("Access-Control-Expose-Headers: X-Custom-header");
+	        config.addExposedHeader("Access-Control-Expose-Headers: Authorization");
+	        config.addAllowedMethod("OPTIONS");
+	        config.addAllowedMethod("GET");
+	        config.addAllowedMethod("POST");
+	        config.addAllowedMethod("PUT");
+	        config.addAllowedMethod("DELETE");
+	        
+	        source.registerCorsConfiguration("/**", config);
+	 
+	        return new CorsFilter(source);
+	    }
+
+	
+}
